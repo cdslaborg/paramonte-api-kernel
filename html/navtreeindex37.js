fa20e472b8e437a04b3b551821974d52b52c4f69
Amir Shahmoradi <a.shahmoradi@gmail.com>
2021-11-12 00:43:07 -0600

    latest build

--- html/navtreeindex37.js
+++ html/navtreeindex37.js
@@ -1,9 +1,10 @@
 var NAVTREEINDEX37 =
 {
-"structTimer__mod_1_1Timer__type.html#ae1a2433e24430fd83a9e8352d0d12776":[5,0,143,2,3],
-"structTimer__mod_1_1Timer__type.html#ae1a2433e24430fd83a9e8352d0d12776":[4,0,234,2,3],
-"structTimer__mod_1_1Timer__type.html#afeb3d00b92dc887cfcd693ad96f1b7b6":[5,0,143,2,5],
-"structTimer__mod_1_1Timer__type.html#afeb3d00b92dc887cfcd693ad96f1b7b6":[4,0,234,2,5],
-"test.html":[1],
-"todo.html":[3]
+"structTimer__mod_1_1Timer__type.html#a883301199e5969b9ea4026e76dac3d56":[5,0,234,2,4],
+"structTimer__mod_1_1Timer__type.html#ae1a2433e24430fd83a9e8352d0d12776":[6,0,143,2,3],
+"structTimer__mod_1_1Timer__type.html#ae1a2433e24430fd83a9e8352d0d12776":[5,0,234,2,3],
+"structTimer__mod_1_1Timer__type.html#afeb3d00b92dc887cfcd693ad96f1b7b6":[6,0,143,2,5],
+"structTimer__mod_1_1Timer__type.html#afeb3d00b92dc887cfcd693ad96f1b7b6":[5,0,234,2,5],
+"test.html":[2],
+"todo.html":[4]
 };
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['bug_20list_0',['Bug List',['../bug.html',1,'']]]
+  ['benchmark_20list_0',['Benchmark List',['../benchmarks.html',1,'']]],
+  ['bug_20list_1',['Bug List',['../bug.html',1,'']]]
 ];
